fe7dd6c04ea8879d533234c0b0f0376c69b8f965
nisten <nisten@outlook.com>
2019-07-29 14:31:28 -0400

    demo

--- site-config.js
+++ site-config.js
@@ -1,16 +1,16 @@
 const path = require('path');
 
 module.exports = {
-  siteTitle: `Gatsby Universal`,
-  siteTitleShort: `GatsbyU`,
-  siteDescription: `An opinionated starter for Gatsby.`,
+  siteTitle: `Perfect Boilerplat`,
+  siteTitleShort: `PerfBoil`,
+  siteDescription: `A high performance gatsby boilerplate`,
   siteUrl: `https://master.d29eszqiyspi5u.amplifyapp.com/`,
   themeColor: `#000`,
   backgroundColor: `#fff`,
   pathPrefix: null,
   logo: path.resolve(__dirname, 'src/images/icon.png'),
   social: {
-    twitter: `gatsbyjs`,
+    twitter: `nis10`,
     fbAppId: `966242223397117`,
   },
 };
